630549e5e2b0faf8c7ee6d34f5cf62ab2f8bd7d8
Pascal Ernst <pascal.cc.ernst@gmail.com>
2016-01-10 02:12:06 +0100

    Add awesome locker-screen

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,8 @@
 [ -d ~/mail/personal ] || mkdir -p ~/mail/personal
 [ -d ~/mail/bl ] || mkdir -p ~/mail/bl
 
+[ -d ~/pictures/icons ] || mkdir -p ~/pictures/icons
+
 [ -d ~/.sup ] || mkdir -p ~/.sup
 
 # Manually link gitignore since stow ignores it
